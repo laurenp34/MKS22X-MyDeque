213b5347e2c19ad76a9fd299022ad894b1787b12
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-21 09:53:16 -0400

    added dummy return values for methods, made default constructor which passes tests + unchecked warning suppressed

--- MyDeque.java
+++ MyDeque.java
@@ -1,16 +1,27 @@
+import java.io.*;
+import java.util.*;
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
 
+
+  @SuppressWarnings("unchecked")
   public MyDeque(){
+    data = (E[]) new Object[10];
   }
   public MyDeque(int initialCapacity){  }
-  public int size(){ }
-  public String toString(){ }
+  public int size(){ return 0;}
+  public String toString(){ return ""; }
   public void addFirst(E element){ }
   public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+  public E removeFirst(E element){ return element; }
+  public E removeLast(E element){  return element; }
+  public E getFirst(E element){ return element;  }
+  public E getLast(E element){  return element; }
+
+  public static void main(String[] args) {
+    MyDeque<String> d = new MyDeque();
+    System.out.println(Arrays.toString(d.data));
+  }
+
 }
