1a191a3db1dbda5fbd4e634abf25fdbda10a7a90
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-03-25 10:09:53 -0400

    started eval method: added splitstring, parseint, and try/catch block but still testing

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,34 @@ public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    public static double eval(String s){}
+    public static double eval(String s){
+      //first, split the string into smaller strings
+      String[] split = s.split(" ");
+      MyDeque<Integer> nums = new MyDeque<Integer>();
+
+      int num = 0;
+      int i=0; // iterate through split array.
+      while (i < split.length) {
+        try {
+          num = Integer.parseInt(split[i]);
+          nums.addLast(num);
+        }catch (NumberFormatException e) {
+          //reaches here if not a number.
+          String cur = split[i];
+          if (cur.equals("+")) {
+            int n1 = nums.getLast();
+            int n2 = nums.getLast();
+            nums.removeLast();
+            nums.removeLast();
+            nums.addLast(n1+n2);
+          }
+        }
+        i++;
+      }
+      return nums.getLast();
+
+  }
+  public static void main(String[] args) {
+    System.out.println(eval("2 3 +"));
+  }
 }
